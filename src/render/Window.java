2833a44ba51f2c2a9904571fc98b74653b9dbff8
Chris <christian.lovato@uni-duesseldorf.de>
2012-05-13 15:44:48 +0200

    kommentiert

--- src/render/Window.java
+++ src/render/Window.java
@@ -29,61 +29,65 @@ public class Window {// implements Runnable {
 		Player player = new Player(level, 50, 50, -150);
 		OpenGL openGl = new OpenGL(level, player, width, height);
 
+		// Programmschleife:
 		while (!Display.isCloseRequested()) {
-
 			openGl.display();
-
 			Display.update();
+			
 			move_Control1(player);
-
 		}
-
 		Display.destroy();
 	}
 
+	
 	public static void main(String[] argv) {
 		Window window = new Window();
 		window.start();
 	}
 
+	// Steuerungskontrolle:
 	public void move_Control1(Player player) {
-
-		// while (Keyboard.next()) {
-		// if (Keyboard.getEventKeyState()) {
+		// links:
 		if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
 			player.moveLeft();
 		}
+		// rückwärts:
 		if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
 			player.moveBackward();
 		}
+		// rechts:
 		if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
 			player.moveRight();
 		}
+		// vorwärts:
 		if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
 			player.moveForward();
 		}
+		// Programm beenden:
 		if (Keyboard.isKeyDown(Keyboard.KEY_T)
 				|| Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
 			Display.destroy();
 			System.exit(0);
 		}
+		// Bombe legen:
 		if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
 			player.setBomb();
 		}
+		// nach rechts drehen:
 		if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
 			player.turnRight();
 		}
+		// nach links drehen:
 		if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
 			player.turnLeft();
 		}
+		// nach unten neigen:
 		if (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
 			player.turnDown();
 		}
+		// nach oben neigen:
 		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
 			player.turnUp();
 		}
-
-		// }
-		// }
 	}
 }
