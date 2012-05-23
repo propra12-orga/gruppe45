00f4f6b73eeb61350390ba5d687591563a5b6da9
felidosz <felix.bandosz@gmx.net>
2012-05-23 10:32:02 +0200

    Dateien gemerget

--- src/control/Control_Mouse.java
+++ src/control/Control_Mouse.java
@@ -1,54 +1,37 @@
 package control;
 
-import org.lwjgl.input.Mouse;
-
 import game.Player;
 
+import org.lwjgl.input.Mouse;
 
 public class Control_Mouse extends Control {
 
 	public Control_Mouse(Player player) {
 		super(player);
-		
-	}
-<<<<<<< HEAD
-	public void MouseMove(Player player){
-		if(Mouse.getX() > 0 ){
-			player.turnLeft();
-			
-		}
-	}
-	   
-	   
-	   
-	   
-   }
-=======
 
+	}
 
 	public void mouse_Move(Player player) {
-		Mouse.setCursorPosition(400,300);
-		float x=0,y=0;
+		Mouse.setCursorPosition(400, 300);
+		float x = 0, y = 0;
 		while (Mouse.next()) {
 			x = x + Mouse.getDX();
 			y = y + Mouse.getDY();
 			if (x > 0) {
-				Mouse.setCursorPosition(400,300);
-				player.turnRight(Math.abs(x)/150);
-			}else if (x < 0){
-				Mouse.setCursorPosition(400,300);
-				player.turnLeft(Math.abs(x)/150);
+				Mouse.setCursorPosition(400, 300);
+				player.turnRight(Math.abs(x) / 150);
+			} else if (x < 0) {
+				Mouse.setCursorPosition(400, 300);
+				player.turnLeft(Math.abs(x) / 150);
 			}
-			if (y < 0){
-				Mouse.setCursorPosition(400,300);
-				player.turnDown(Math.abs(y)/150);
-			}else if(y>0){
-				Mouse.setCursorPosition(400,300);
-				player.turnUp(Math.abs(y)/150);
+			if (y < 0) {
+				Mouse.setCursorPosition(400, 300);
+				player.turnDown(Math.abs(y) / 150);
+			} else if (y > 0) {
+				Mouse.setCursorPosition(400, 300);
+				player.turnUp(Math.abs(y) / 150);
 			}
 		}
 
 	}
 }
-
->>>>>>> 671f5902b8de7aadbcdac2991effabef57ecfa4d
--- src/render/Window.java
+++ src/render/Window.java
@@ -2,19 +2,14 @@ package render;
 
 import game.Level;
 import game.Player;
-<<<<<<< HEAD
-import control.Control_Keyboard;
-import control.Control_Mouse;
-
-=======
 import game.cube.CubeExit;
->>>>>>> 671f5902b8de7aadbcdac2991effabef57ecfa4d
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
-import control.Control_Mouse;
 
 import control.Control_Keyboard;
+import control.Control_Mouse;
 
 public class Window {// implements Runnable {
 
@@ -34,22 +29,15 @@ public class Window {// implements Runnable {
 			System.exit(0);
 		}
 		Level level = new Level();
-		level.setCube(new CubeExit(), 2, 2, 8);				//POSITION DES AUSGANGS
-		Player player = new Player(level, 85, 85, 15); 		//STARTPOSITION SPIELER
+		level.setCube(new CubeExit(), 2, 2, 8); // POSITION DES AUSGANGS
+		Player player = new Player(level, 85, 85, 15); // STARTPOSITION SPIELER
 		OpenGL openGl = new OpenGL(level, player, width, height);
 		Control_Keyboard keyboard = new Control_Keyboard(player);
-<<<<<<< HEAD
 		Control_Mouse mouse = new Control_Mouse(player);
-		
-=======
-		Control_Mouse mouse  = new Control_Mouse(player);
-
->>>>>>> 671f5902b8de7aadbcdac2991effabef57ecfa4d
 		// Programmschleife:
 		while (!Display.isCloseRequested()) {
 			openGl.display();
 			Display.update();
-			mouse.MouseMove(player);
 			keyboard.move_Control1(player);
 			mouse.mouse_Move(player);
 		}
