a62a6e595b83b636d045d284f4a056e85fcfbe98
felidosz <felix.bandosz@uni-duesseldorf.de>
2012-06-14 15:09:35 +0200

    meilensteine

--- src/game/Level.java
+++ src/game/Level.java
@@ -18,11 +18,12 @@ import java.util.Scanner;
 public class Level {
 
 	// TODO Menüoption für ObstacleProbability
-	
+
 	/**
-	 * Wahrscheinlichkeit, dass bei zufälliger Levelfüllung ein Hindernis an eine freie Stelle gesetzt wird
+	 * Wahrscheinlichkeit, dass bei zufälliger Levelfüllung ein Hindernis an
+	 * eine freie Stelle gesetzt wird
 	 */
-	final static public int OBSTACLE_PROBABILITY = 45;  // Wahrscheinlichkeit
+	final static public int OBSTACLE_PROBABILITY = 45; // Wahrscheinlichkeit
 														// eines Hindernisses
 														// an leerer Stelle des
 														// Levels (0..100 %)
@@ -31,12 +32,12 @@ public class Level {
 	// TODO Menüintegration
 	final static public byte THEME_EARTH = 1;
 	final static public byte THEME_SPACE = 2;
-	
+
 	/**
 	 * Umschalten zwischen Darstellungsthemen
 	 */
-	byte themeSelection = THEME_EARTH;
-	// byte themeSelection = THEME_SPACE;
+	// byte themeSelection = THEME_EARTH;
+	byte themeSelection = THEME_SPACE;
 
 	/**
 	 * Ermöglicht das Verstecken des Ausgangs in einem zerstörbaren Block
